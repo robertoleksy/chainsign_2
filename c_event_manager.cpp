22ce6446af81aa73e04754184e0d63b081531901
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-28 13:16:11 +0000

    init

---
+++ c_event_manager.cpp
@@ -0,0 +1,6 @@
+#include "c_event_manager.hpp"
+
+c_event_manager::c_event_manager()
+{
+
+}
---
+++ c_event_manager.hpp
@@ -0,0 +1,11 @@
+#ifndef C_EVENT_MANAGER_HPP
+#define C_EVENT_MANAGER_HPP
+
+
+class c_event_manager
+{
+	public:
+		c_event_manager();
+};
+
+#endif // C_EVENT_MANAGER_HPP
