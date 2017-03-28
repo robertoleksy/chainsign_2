c8aeaa62ab6e6b6401e6767f500e5c1c387ccc21
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-28 14:40:44 +0000

    [wip] open watch descriptor

--- c_event_manager.cpp
+++ c_event_manager.cpp
@@ -1,6 +1,19 @@
 #include "c_event_manager.hpp"
+#include <sys/inotify.h>
 
-c_event_manager::c_event_manager()
+c_event_manager::c_event_manager(const std::string &path_name) :
+	m_fd(init_fd(path_name)),
+	m_io_service(),
+	m_dir_fd(m_io_service)
 {
+}
 
+int c_event_manager::init_fd(const std::string &path_name) {
+	int fd = inotify_init();
+	if (fd == -1) throw std::runtime_error("inotify_init error");
+	int wd = inotify_add_watch(fd, path_name.c_str(), IN_CREATE);
+	if (wd == -1) throw std::runtime_error("inotify_add_watch error");
+
+
+	return 0;
 }
--- c_event_manager.hpp
+++ c_event_manager.hpp
@@ -1,11 +1,17 @@
 #ifndef C_EVENT_MANAGER_HPP
 #define C_EVENT_MANAGER_HPP
 
+#include <boost/asio.hpp>
+#include <string>
 
-class c_event_manager
-{
+class c_event_manager final {
 	public:
-		c_event_manager();
+		c_event_manager(const std::string &path_name);
+	private:
+		int m_fd;
+		boost::asio::io_service m_io_service;
+		boost::asio::posix::stream_descriptor m_dir_fd;
+		int init_fd(const std::string &path_name);
 };
 
 #endif // C_EVENT_MANAGER_HPP
